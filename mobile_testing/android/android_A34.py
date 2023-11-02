7f7e9efc28bb3ccc06f25401fcbccf5efd8109e5
Andrey Stoletov <stoletov314@gmail.com>
2023-11-02 22:22:51 +0100

    some minor changing

--- mobile_testing/android/android_A34.py
+++ mobile_testing/android/android_A34.py
@@ -1,41 +1,39 @@
 import pytest
+import unittest
 from appium import webdriver
-from appium.webdriver.common.mobileby import MobileBy
+from appium.webdriver.common.appiumby import AppiumBy
+from selenium import webdriver as selenium_webdriver
 
 # Define the test data as a list of search queries
 search_queries = ["Appium", "Mobile Testing", "Automation"]
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="function")       # First fixture
 def appium_driver():
-    # Desired capabilities for the Android device and app
+    # Short describe of capabilities
     desired_capabilities = {
         'platformName': 'Android',
         'deviceName': 'A34',
         'automationName': 'UiAutomator2',
         'app': '/Users/andrey/Downloads/app-development-release (1).apk',
+        'udid': "RZCW711MGVY",  # Must be change for your UDID for ANDROID
+        "noReset": "true"
     }
 
-    # Initialize the Appium driver
+    # Start the Appium driver
     driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_capabilities)
     yield driver
     driver.quit()
 
 
-@pytest.mark.parametrize("search_query", search_queries)
+@pytest.mark.parametrize("search_query", search_queries)        # Parametrise decorator
 def test_search_and_click_button(appium_driver, search_query):
-    # Find and interact with elements in the app
-    search_field = appium_driver.find_element(MobileBy.ID, 'com.example.app:id/search_field')
-    search_field.send_keys(search_query)
-
-    search_button = appium_driver.find_element(MobileBy.ID, 'com.example.app:id/search_button')
-    search_button.click()
-
     # Find the "I already have account" button using the specified XPath
     already_have_account_button = appium_driver.find_element(
-        MobileBy.XPATH, '//android.widget.Button[@content-desc="I already have account"]'
+        AppiumBy.XPATH, '//android.widget.Button[@content-desc="I already have account"]'
     )
     already_have_account_button.click()
 
     # Close the app
     appium_driver.close_app()
+
