dceb0b7b7106d9590f6cf292933e8f133aa97e7b
Andrey Stoletov <stoletov314@gmail.com>
2023-11-02 20:02:29 +0100

    first test attemt

--- mobile_testing/android_A34.py
+++ mobile_testing/android_A34.py
@@ -1,20 +1,39 @@
+import pytest
 from appium import webdriver
-from appium.webdriver.appium_service import AppiumService
+from appium.webdriver.common.mobileby import MobileBy
 
-appium_service = AppiumService()
+# Define the test data as a list of search queries
+search_queries = ["Appium", "Mobile Testing", "Automation"]
 
-appium_capabilities = {
-    'automationName': 'UiAutomator2',  # Use UiAutomator2 for Android automation
-    'platformName': 'Android',
-    'udid': 'RZCW711MGVY',
-    'deviceName': 'A34',  # Example: 'Pixel 4' or 'emulator-5554'
-    'app': '/Users/andrey/Downloads/app-development-release (1).apk',
-    # 'appPackage': 'io.appium.android.apis',  # Example: 'com.example.myapp'
-    # 'appActivity': '.ApiDemos',
-}
 
-# Initialize the Appium driver for Android
-appium_driver = webdriver.Remote(
-    command_executor='http://localhost:4723/wd/hub',
-    desired_capabilities=appium_capabilities
-)
+@pytest.fixture(scope="function")
+def appium_driver():
+    # Desired capabilities for the Android device and app
+    desired_capabilities = {
+        'platformName': 'Android',
+        'deviceName': 'A34',
+        'automationName': 'UiAutomator2',
+        'app': '/Users/andrey/Downloads/app-development-release (1).apk',
+    }
+
+    # Initialize the Appium driver
+    driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_capabilities)
+    yield driver
+    driver.quit()
+
+
+@pytest.mark.parametrize("search_query", search_queries)
+def test_search(appium_driver, search_query):
+    # Find and interact with elements in the app
+    search_field = appium_driver.find_element(MobileBy.ID, 'com.example.app:id/search_field')
+    search_field.send_keys(search_query)
+
+    search_button = appium_driver.find_element(MobileBy.ID, 'com.example.app:id/search_button')
+    search_button.click()
+
+    # # Perform a swipe on the screen
+    # action = TouchAction(appium_driver)
+    # action.press(x=500, y=1500).move_to(x=500, y=500).release().perform()
+
+    # Close the app
+    appium_driver.close_app()
