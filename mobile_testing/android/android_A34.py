f31c1fb8139f94bf4a4c71ad16101d46d8da22d5
Andrey Stoletov <stoletov314@gmail.com>
2023-11-03 02:52:24 +0100

    new values , attempt to going deeply through test scenario

--- mobile_testing/android/android_A34.py
+++ mobile_testing/android/android_A34.py
@@ -30,10 +30,13 @@ def driver():
 def test_example(driver) -> None:
     el = driver.find_element(by=AppiumBy.XPATH, value='//android.widget.Button[@content-desc="I already have account"]')
     el_1 = driver.find_element(by=AppiumBy.XPATH, value='//android.widget.FrameLayout[@resource-id="android:id/content"]/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View')
-    # el_2 = driver.find_element(by=AppiumBy.XPATH, value='//android.widget.Button[@content-desc="Continue"]')
+    # el_2 = driver.find_element(by=AppiumBy.ID, value='00000000-0000-051b-0000-003c00000004')
     # el_3 = driver.find_element(by=AppiumBy.XPATH, value='//android.widget.FrameLayout[@resource-id="android:id/content"]')
     el.click()
-    sleep(5)
+    # el_1.click()
+    # el_2.click()
+    # el_3.click()
+    sleep(10)
 
 
 
