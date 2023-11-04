b856fa0f9889109880e1181bf231c6e5e828ba84
Andrey Stoletov <stoletov314@gmail.com>
2023-11-04 03:44:19 +0100

    attempt to implement password input in the app

--- mobile_testing/android/android_A34.py
+++ mobile_testing/android/android_A34.py
@@ -6,6 +6,11 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from appium.options.android import UiAutomator2Options
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.actions import interaction
+from selenium.webdriver.common.actions.action_builder import ActionBuilder
+from selenium.webdriver.common.actions.pointer_input import PointerInput
+
 
 # Desired capabilities to specify the Android device and app details
 appium_capabilities = {
@@ -41,11 +46,20 @@ def test_click_already_have_account_button(driver) -> None:
     locator_2 = '//android.widget.Button[@content-desc="Continue"]'
     el_2 = driver.find_element(by=AppiumBy.XPATH, value=locator_2)
     el_2.click()
-    # locator_3 = '00000000-0000-0c2f-0000-002900000004' Does not work
-    # locator_4 = '00000000-0000-0c2f-0000-002b00000004'
-    # el_3 = driver.find_element(by=AppiumBy.XPATH, value=locator_3)
-    # el_4 = driver.find_element(by=AppiumBy.XPATH, value=locator_4)
-    # el_3.send_keys('123456')
-    # el_4.send_keys('123456')
-    # sleep(5)
+    locator_3 = '00000000-0000-0ccf-0000-002b00000004'
+    locator_4 = '00000000-0000-0ccf-0000-002b00000004'
+    password_field = driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value=locator_3)
+    password_field_1 = driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value=locator_4)
+    password_field.click()
+    password_field.clear()
+    # Input the password in the first input field
+    password_field.send_keys('123456')
 
+    # Input the password in the second input field
+    password_field_1.send_keys('123456')
+
+    sleep(5)
+
+
+if __name__ == '__main__':
+    pytest.main()
