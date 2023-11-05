772f5f12d43e59a1c4629388505c63b778fb4d82
Andrey Stoletov <stoletov314@gmail.com>
2023-11-05 02:54:56 +0100

    successfull implementation of password

--- mobile_testing/android/android_A34.py
+++ mobile_testing/android/android_A34.py
@@ -19,6 +19,7 @@ appium_capabilities = {
     'udid': 'RZCW711MGVY',
     'deviceName': 'A34',  # Example: 'Pixel 4' or 'emulator-5554'
     'app': '/Users/andrey/Downloads/app-development-release (1).apk',
+    'appWaitForLaunch': 'false',
 }
 
 appium_capabilities = UiAutomator2Options().load_capabilities(appium_capabilities)
@@ -34,7 +35,7 @@ def driver():
         android_driver.quit()
 
 
-def test_click_already_have_account_button(driver):
+def test_login(driver):
     driver.implicitly_wait(20)
     locator = '//android.widget.Button[@content-desc="I already have account"]'
     driver.find_element(by=AppiumBy.XPATH, value=locator).click()
@@ -42,30 +43,17 @@ def test_click_already_have_account_button(driver):
     driver.find_element(by=AppiumBy.XPATH, value=locator_1).click()
     locator_2 = '//android.widget.Button[@content-desc="Continue"]'
     driver.find_element(by=AppiumBy.XPATH, value=locator_2).click()
-    driver.find_element(AppiumBy.XPATH, value='//android.widget.ScrollView/android.widget.EditText[1]').send_keys('123456')
+    wait = WebDriverWait(driver, 20)
+    locator_3 = '//android.widget.FrameLayout[@resource-id="android:id/content"]/android.widget.FrameLayout/android' \
+                '.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android' \
+                '.view.View/android.view.View[2]/android.view.View/android.widget.EditText[1] '
+    locator_4 = '//android.widget.FrameLayout[@resource-id="android:id/content"]/android.widget.FrameLayout/android' \
+                '.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android' \
+                '.view.View/android.view.View[2]/android.view.View/android.widget.EditText[2] '
+    element = wait.until(EC.presence_of_element_located((AppiumBy.XPATH, locator_3)))
+    element.click()
+    element.send_keys("123456")
+    element_1 = wait.until(EC.presence_of_element_located((AppiumBy.XPATH, locator_4)))
+    element_1.click()
+    element_1.send_keys("123456")
 
-
-# def login_password(driver):
-#     driver.find_element(AppiumBy.ID, value='00000000-0000-0e0f-0000-002900000004').click()
-#     sleep(5)
-
-#     locator_3 = 'android.widget.EditText'
-#     locator_4 = '//android.widget.ScrollView/android.widget.EditText[2]'
-#
-#     wait = WebDriverWait(driver, 10)
-#
-#     password_field = driver.find_element(by=AppiumBy.CLASS_NAME, value=locator_3)
-#     password_field_1 = driver.find_element(by=AppiumBy.XPATH, value=locator_4)
-#     password_field.click()
-#     # Input the password in the first input field
-#
-#     password_field.click()
-#     password_field.send_keys('123456')
-#     password_field_1.click()
-#     password_field_1.send_keys('123456')
-#
-#     sleep(5)
-#
-#
-# if __name__ == '__main__':
-#     pytest.main()
