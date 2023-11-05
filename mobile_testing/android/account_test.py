6a80a4730038c0fdef6f5d5d46d5b80e04c67f5f
Andrey Stoletov <stoletov314@gmail.com>
2023-11-05 03:51:33 +0100

    test for account, basic setup

---
+++ mobile_testing/android/account_test.py
@@ -0,0 +1,36 @@
+import pytest
+from appium import webdriver
+from appium.webdriver.common.appiumby import AppiumBy
+from time import sleep
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from appium.options.android import UiAutomator2Options
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.actions import interaction
+from selenium.webdriver.common.actions.action_builder import ActionBuilder
+from selenium.webdriver.common.actions.pointer_input import PointerInput
+import android_A34
+
+
+# Desired capabilities to specify the Android device and app details
+appium_capabilities = {
+    'automationName': 'UiAutomator2',  # Use UiAutomator2 for Android automation
+    'platformName': 'Android',
+    'udid': 'RZCW711MGVY',
+    'deviceName': 'A34',  # Example: 'Pixel 4' or 'emulator-5554'
+    'app': '/Users/andrey/Downloads/app-development-release (1).apk',
+    'appWaitForLaunch': 'false',
+}
+
+appium_capabilities = UiAutomator2Options().load_capabilities(appium_capabilities)
+appium_server_url = 'http://localhost:4723/wd/hub'
+
+
+# Initialize the Appium driver for Android using a fixture
+@pytest.fixture()
+def driver():
+    android_driver = webdriver.Remote(appium_server_url, options=appium_capabilities)
+    yield android_driver
+    if android_driver:
+        android_driver.quit()
