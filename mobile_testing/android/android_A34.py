afeb78a4df92865891c93f06b072db12e9804357
Andrey Stoletov <stoletov314@gmail.com>
2023-11-05 04:41:52 +0100

    minor fixes

--- mobile_testing/android/android_A34.py
+++ mobile_testing/android/android_A34.py
@@ -3,13 +3,13 @@ from appium import webdriver
 from appium.webdriver.common.appiumby import AppiumBy
 from time import sleep
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
 from appium.options.android import UiAutomator2Options
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.actions import interaction
-from selenium.webdriver.common.actions.action_builder import ActionBuilder
-from selenium.webdriver.common.actions.pointer_input import PointerInput
+# from selenium.webdriver.support import expected_conditions as EC
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.common.action_chains import ActionChains
+# from selenium.webdriver.common.actions import interaction
+# from selenium.webdriver.common.actions.action_builder import ActionBuilder
+# from selenium.webdriver.common.actions.pointer_input import PointerInput
 
 # Desired capabilities to specify the Android device and app details
 appium_capabilities = {
