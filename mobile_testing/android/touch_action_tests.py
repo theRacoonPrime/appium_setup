fdb57dfdc445734a73c43d1ac334b35805167fe3
Andrey Stoletov <stoletov314@gmail.com>
2023-11-27 12:02:15 +0100

    Minor fix and set up

--- mobile_testing/android/touch_action_tests.py
+++ mobile_testing/android/touch_action_tests.py
@@ -1,11 +1,74 @@
 import pytest
 from selenium.common.exceptions import NoSuchElementException
-
+from appium import webdriver
 from appium.webdriver.common.appiumby import AppiumBy
 from appium.webdriver.common.touch_action import TouchAction
 from test_helper import wait_for_element
+from selenium.webdriver.support.ui import WebDriverWait
 from test_helper import APIDEMO_PKG_NAME, BaseTestCase, is_ci
+from appium.options.android import UiAutomator2Options
+import json
+from selenium.webdriver.support import expected_conditions as EC
 
+
+# Load locators from JSON file
+@pytest.fixture
+def load_locators():
+    with open('/Users/andrey/Desktop/appium_setup/mobile_testing/android/card_and_stikcers_data.json') as f:
+        return json.load(f)
+
+
+# Desired capabilities to specify the Android device and app details
+@pytest.fixture
+def appium_capabilities():
+    # Load appium_capabilities from JSON file
+    with open('/Users/andrey/Desktop/appium_setup/mobile_testing/android/capabilities.json') as f:
+        capabilities = json.load(f)
+
+    # Configure appium_capabilities using UiAutomator2Options
+    capabilities = UiAutomator2Options().load_capabilities(capabilities)
+
+    return capabilities
+
+
+# Appium server url
+@pytest.fixture
+def appium_server_url():
+    return 'http://localhost:4723/wd/hub'
+
+
+@pytest.fixture
+def driver(appium_capabilities, appium_server_url):
+    driver = webdriver.Remote(appium_server_url, options=appium_capabilities)
+    yield driver
+    if driver:
+        driver.quit()
+
+
+# Common actions
+def wait_and_click(driver, locator):
+    element = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((AppiumBy.XPATH, locator)))
+    element.click()
+
+
+def enter_text_and_hide_keyboard(driver, locator, text):
+    element = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((AppiumBy.XPATH, locator)))
+    element.click()
+    element.send_keys(text)
+    driver.hide_keyboard()
+
+
+# Swipe action
+def swipe(driver):
+    # Define swipe coordinates (adjust as needed)
+    start_x = 300
+    start_y = 500
+    end_x = 100
+    end_y = 100
+    duration = 1000  # Duration in milliseconds
+
+    # Perform the swipe action
+    driver.swipe(start_x, start_y, end_x, end_y, duration)
 
 def test_tap(self) -> None:
     el = self.driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value='Animation')
