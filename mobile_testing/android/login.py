fc75dbfac7f8842b87fd3d8469f67c0c1ae12668
Andrey Stoletov <stoletov314@gmail.com>
2023-12-11 09:57:43 +0100

    Negative test fixture was added for login

--- mobile_testing/android/login.py
+++ mobile_testing/android/login.py
@@ -1,6 +1,5 @@
 import json
 import time
-
 import pytest
 from appium import webdriver
 from appium.webdriver.common.appiumby import AppiumBy
@@ -41,3 +40,33 @@ def test_login(driver, perform_actions_with_wait, load_locators):
 
     perform_actions_with_wait(actions)
 
+
+@pytest.fixture
+def negative_test_login(driver, perform_actions_with_wait, load_locators):
+    locators_data = load_locators  # Use it as a fixture, not a function
+    driver.implicitly_wait(20)
+
+    # Add a sleep time before the test actions
+    sleep_time_before_actions = 14  # Adjust the duration as needed
+    time.sleep(sleep_time_before_actions)
+
+    actions = [
+        # Login to the app with password input
+        {'action': 'wait_and_click', 'locator': locators_data['already_have_account']},
+        {'action': 'wait_and_click', 'locator': locators_data['continue_button']},
+        {'action': 'wait_and_click', 'locator': locators_data['continue_button']},
+        {'action': 'enter_text_and_hide_keyboard', 'locator': locators_data['password_field_1'], 'text': '123456'},
+        {'action': 'enter_text_and_hide_keyboard', 'locator': locators_data['password_field_2'], 'text': '123456'},
+        {'action': 'wait_and_click', 'locator': locators_data['continue_button']},
+        {'action': 'wait_and_click', 'locator': locators_data['thanks_button']},
+        {'action': 'enter_text_and_hide_keyboard', 'locator': locators_data['device_name_input'], 'text':
+            'bkdk2323e23232232134324231432423rfdsfdsgdfsgfdgnm/.,mewwedsf.z.x.lllsdkdfgldkjsj '
+            'jdjmndfnfnvdnfndmsfsdkfksdmvkdfnmmnmfdsnglkgsdjrijitu57847584tindfjfg'},
+        {'action': 'wait_and_click', 'locator': locators_data['continue_button']},
+        {'action': 'wait_and_click', 'locator': locators_data['go_to_app_button']},
+        {'action': 'wait_and_click', 'locator': locators_data['password_field_general']},
+        {'action': 'enter_text_and_hide_keyboard', 'locator': locators_data['password_field_general'], 'text': 'bkdkjfg'},
+        {'action': 'wait_and_click', 'locator': locators_data['accept_button']}
+     ]
+
+    perform_actions_with_wait(actions)
--- mobile_testing/android/test_collection.py
+++ mobile_testing/android/test_collection.py
@@ -3,7 +3,7 @@ from test_helper import( wait_and_click, appium_server_url, wait_for_element,
                          swipe, driver, enter_text_and_hide_keyboard, perform_actions_with_wait, appium_capabilities,
                          load_locators_card, load_locators)
 from Card_and_Stikers import test_cards
-from login import test_login
+from login import test_login, negative_test_login
 from account_test import payment
 from current_account import current_account
 
@@ -14,15 +14,19 @@ def test_scenario_1(test_login):
 
 
 @pytest.mark.run(order=2)
-def test_scenario_2(test_login, test_cards):
+def test_scenario_2(negative_test_login):
     assert True
 
-
-@pytest.mark.run(order=3)
-def test_scenario_3(test_login, payment):
-    assert True
-
-
-@pytest.mark.run(order=4)
-def test_scenario_4(test_login, current_account):
-    assert True
+# @pytest.mark.run(order=2)
+# def test_scenario_2(test_login, test_cards, test_cards_negative):
+#     assert True
+#
+#
+# @pytest.mark.run(order=3)
+# def test_scenario_3(test_login, payment):
+#     assert True
+#
+#
+# @pytest.mark.run(order=4)
+# def test_scenario_4(test_login, current_account):
+#     assert True
