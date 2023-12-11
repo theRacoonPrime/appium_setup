b919911cee5ff1211a78a1d73873ff8089d89466
Andrey Stoletov <stoletov314@gmail.com>
2023-12-11 07:40:45 +0100

    Minor fix

--- mobile_testing/android/account_test.py
+++ mobile_testing/android/account_test.py
@@ -18,19 +18,6 @@ def payment(driver, perform_actions_with_wait, load_locators):
     driver.implicitly_wait(20)
 
     actions = [
-        # Login to the app with password input
-        # {'action': 'wait_and_click', 'locator': locators_data['already_have_account']},
-        # {'action': 'wait_and_click', 'locator': locators_data['continue_button']},
-        # {'action': 'wait_and_click', 'locator': locators_data['continue_button']},
-        # {'action': 'enter_text_and_hide_keyboard', 'locator': locators_data['password_field_1'], 'text': '123456'},
-        # {'action': 'enter_text_and_hide_keyboard', 'locator': locators_data['password_field_2'], 'text': '123456'},
-        # {'action': 'wait_and_click', 'locator': locators_data['continue_button']},
-        # {'action': 'wait_and_click', 'locator': locators_data['thanks_button']},
-        # {'action': 'wait_and_click', 'locator': locators_data['device_name']},
-        # {'action': 'wait_and_click', 'locator': locators_data['go_to_app_button']},
-        # {'action': 'wait_and_click', 'locator': locators_data['password_field_general']},
-        # {'action': 'enter_text_and_hide_keyboard', 'locator': locators_data['password_field_general'], 'text': '123456'},
-        # {'action': 'wait_and_click', 'locator': locators_data['accept_button']},
         {'action': 'wait_and_click', 'locator': locators_data['acc_button']},
         {'action': 'wait_and_click', 'locator': locators_data['new_payment']},
         # # New payment was clicked
