94098d14ca1b04d0a37434e8eeca2f079aee4510
Andrey Stoletov <stoletov314@gmail.com>
2023-12-17 16:02:06 +0100

    Docker files were comments

---
+++ mobile_testing/android/account_test.py
@@ -0,0 +1,34 @@
+import json
+import pytest
+from appium import webdriver
+from appium.webdriver.common.appiumby import AppiumBy
+from time import sleep
+from selenium.webdriver.support.ui import WebDriverWait
+from appium.options.android import UiAutomator2Options
+from selenium.webdriver.support import expected_conditions as EC
+from appium.webdriver.common.touch_action import TouchAction
+from test_helper import( wait_and_click, appium_server_url, wait_for_element, load_locators,
+                         swipe, driver, enter_text_and_hide_keyboard, perform_actions_with_wait, appium_capabilities)
+
+
+# Test using the fixtures
+@pytest.fixture
+def payment(driver, perform_actions_with_wait, load_locators):
+    locators_data = load_locators  # Use it as a fixture, not a function
+    driver.implicitly_wait(20)
+
+    actions = [
+    ]
+
+    perform_actions_with_wait(actions)
+
+
+@pytest.fixture
+def payment_negative_test(driver, perform_actions_with_wait, load_locators):
+    locators_data = load_locators  # Use it as a fixture, not a function
+    driver.implicitly_wait(20)
+
+    actions = [
+    ]
+
+    perform_actions_with_wait(actions)
---
+++ mobile_testing/android/transfers_test.py
@@ -0,0 +1,45 @@
+import json
+import pytest
+from appium import webdriver
+from appium.webdriver.common.appiumby import AppiumBy
+from time import sleep
+from selenium.webdriver.support.ui import WebDriverWait
+from appium.options.android import UiAutomator2Options
+from selenium.webdriver.support import expected_conditions as EC
+from appium.webdriver.common.touch_action import TouchAction
+from test_helper import( wait_and_click, appium_server_url,
+                         wait_for_element,
+                         swipe, driver, enter_text_and_hide_keyboard,
+                         perform_actions_with_wait,
+                         appium_capabilities,
+                         load_locators_card, load_locators)
+
+
+# Test using the fixtures
+@pytest.fixture
+def test_transfer(driver, perform_actions_with_wait, load_locators):
+    locators_data = load_locators  # Use it as a fixture, not a function
+    driver.implicitly_wait(20)
+
+    actions = [
+        # Login to the app with password input
+        # New payment was clicked
+        # Choose acc from button
+    ]
+
+    perform_actions_with_wait(actions)
+
+
+@pytest.fixture
+def test_transfer_negative(driver, perform_actions_with_wait, load_locators):
+    locators_data = load_locators  # Use it as a fixture, not a function
+    driver.implicitly_wait(20)
+
+    actions = [
+        # Login to the app with password input
+        # New payment was clicked
+        # Choose acc from button
+
+    ]
+
+    perform_actions_with_wait(actions)
