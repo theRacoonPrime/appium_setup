60c29eb876a406c75df1a68ed3886ec362f3f0ef
Andrey Stoletov <stoletov314@gmail.com>
2024-02-10 21:47:13 +0100

    Login rewriting, test data rewriting

--- mobile_testing/android/login.py
+++ mobile_testing/android/login.py
@@ -16,21 +16,23 @@ from test_helper import( wait_and_click, appium_server_url, wait_for_element, lo
 @pytest.fixture
 def test_login(driver, perform_actions_with_wait, load_locators):
     locators_data = load_locators  # Use it as a fixture, not a function
-    driver.implicitly_wait(15)
+    driver.implicitly_wait(5)
 
     # Add a sleep time before the test actions
-    sleep_time_before_actions = 12  # Adjust the duration as needed
+    sleep_time_before_actions = 1  # Adjust the duration as needed
     time.sleep(sleep_time_before_actions)
 
     actions = [
         # Login to the app with password input
         {'action': 'wait_and_click', 'locator': locators_data['already_have_account']},
         {'action': 'wait_and_click', 'locator': locators_data['continue_button']},
+        {'action': 'enter_text_and_hide_keyboard', 'locator': locators_data['Sms_code_button'], 'text': '111111'},
         {'action': 'wait_and_click', 'locator': locators_data['continue_button']},
         {'action': 'enter_text_and_hide_keyboard', 'locator': locators_data['password_field_1'], 'text': '123456'},
         {'action': 'enter_text_and_hide_keyboard', 'locator': locators_data['password_field_2'], 'text': '123456'},
         {'action': 'wait_and_click', 'locator': locators_data['continue_button']},
         {'action': 'wait_and_click', 'locator': locators_data['thanks_button']},
+        {'action': 'wait_and_click', 'locator': locators_data['confirm_button']},
         {'action': 'wait_and_click', 'locator': locators_data['device_name']},
         {'action': 'wait_and_click', 'locator': locators_data['go_to_app_button']},
         {'action': 'wait_and_click', 'locator': locators_data['password_field_general']},
